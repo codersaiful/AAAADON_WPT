1a5722711617b3280031d64821c664a72ae7ebac
zbandhan <zbandhan@gmail.com>
2022-10-21 00:46:49 +0600

    Old codes are deleted

--- featured.js
+++
@@ -1,9 +0,0 @@
-;(function($){
-    $(function(){
-        // Pull all the featured products at the first place of the table.
-        var _featured_top = $("table.wpt_product_table tr.featured-top");
-        if( _featured_top.length ) {
-            _featured_top.prependTo("table.wpt_product_table tbody");
-        }
-    });
-}(jQuery));
